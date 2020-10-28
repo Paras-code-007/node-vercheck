5f835381937275de7207a614be7b4241d2d86cc2
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-28 19:22:40 +0530

    update test.js gitignore file

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const nodecheckver = require("./index.js");
 
-// nodecheckver('14',{})
-// nodecheckver("14", { exit: false });
+nodecheckver('14',{})
+nodecheckver("14", { exit: false });
 nodecheckver("14", { exit: true });
+nodecheckver('10')
